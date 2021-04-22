ed76b8db7ad1015fad683414595f1d20729f1596
Marc Heiduk <marc.heiduk@geizhals.at>
2021-04-22 13:13:11 +0200

    add router

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/ghshop-components/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
