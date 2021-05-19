d34c3bf2f7a81c89473e4df3a2323546ffcefe6f
mheiduk <mheiduk@users.noreply.github.com>
2021-05-19 09:11:22 +0000

    deploy: 186ce60fc1db3eaec85dc5308a02f453fb2ed768

---
+++ _nuxt/static/1621415481/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fbrands","\u002Fbutton","\u002Fcentered-text","\u002Ffooter","\u002Fgallery-grid","\u002Fhero","\u002Fparagraph-image","\u002Fslider","\u002Fproducts-grid"]})
