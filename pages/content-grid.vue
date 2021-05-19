72bfa00b3804515bb8f93a174466dd147cad9499
Marc Heiduk <marc.heiduk@geizhals.at>
2021-05-19 15:03:35 +0200

    add content-grid

---
+++ pages/content-grid.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <section>
+      <div class="px-5 py-8 mx-auto">
+        <div class="flex flex-wrap">
+          <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+            <div class="rounded-lg h-64 overflow-hidden">
+              <img alt="content" class="object-cover object-center h-full w-full" src="~/assets/placeholder-450x300.jpg">
+            </div>
+            <h2 class="text-xl font-medium title-font mt-5">Shooting Stars</h2>
+            <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+            <a href="#" class="inline-flex items-center mt-3">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 mr-2" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+              Learn More
+            </a>
+          </div>
+          <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+            <div class="rounded-lg h-64 overflow-hidden">
+              <img alt="content" class="object-cover object-center h-full w-full" src="~/assets/placeholder-450x300.jpg">
+            </div>
+            <h2 class="text-xl font-medium title-font mt-5">The Catalyzer</h2>
+            <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+            <a href="#" class="inline-flex items-center mt-3">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 mr-2" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+              Learn More
+            </a>
+          </div>
+          <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+            <div class="rounded-lg h-64 overflow-hidden">
+              <img alt="content" class="object-cover object-center h-full w-full" src="~/assets/placeholder-450x300.jpg">
+            </div>
+            <h2 class="text-xl font-medium title-font mt-5">The 400 Blows</h2>
+            <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+            <a href="#" class="inline-flex items-center mt-3">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 mr-2" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    layout: 'components',
+    data() {
+      return {
+        title: 'Content Grid',
+      }
+    },
+    mounted() {
+      this.$store.commit('setTitle', this.title)
+    },
+  }
+</script>
+
+<style>
+  .gh-button {
+    color: var(--buttonText);
+
+    @apply
+      bg-primary
+      hover:bg-secondary
+      px-4
+      py-2
+      mt-4
+      rounded
+      no-underline
+  }
+</style>
