2c8e3721bb1f587efb4140487a505c08c36abc92
Marc Heiduk <marc.heiduk@geizhals.at>
2021-08-27 14:06:37 +0200

    add splidejs slider

--- pages/slider.vue
+++ pages/slider.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <div>Slider</div>
+    <splide :options="options">
+      <splide-slide>
+        <a href="#">
+          <img src="~/assets/placeholder-1280x400.jpg" alt="">
+        </a>
+      </splide-slide>
+      <splide-slide>
+        <a href="#">
+          <img src="~/assets/placeholder-1280x400-2.jpg" alt="">
+        </a>
+      </splide-slide>
+      <splide-slide>
+        <a href="#">
+          <img src="~/assets/placeholder-1280x400-3.jpg" alt="">
+        </a>
+      </splide-slide>
+      <splide-slide>
+        <a href="#">
+          <img src="~/assets/placeholder-1280x400-4.jpg" alt="">
+        </a>
+      </splide-slide>
+    </splide>
   </div>
 </template>
 
@@ -9,7 +30,13 @@
     layout: 'components',
     data() {
       return {
-        title: 'Slider'
+        title: 'Slider',
+        options: {
+			  	rewind : true,
+				  perPage: 1,
+				  gap: '1rem',
+          autoplay: true,
+			  },
       }
     },
     mounted() {
---
+++ plugins/vue-splide.client.js
@@ -0,0 +1,5 @@
+import VueSplide from '@splidejs/vue-splide'
+import Vue from 'vue'
+import '@splidejs/splide/dist/css/themes/splide-default.min.css'
+
+Vue.use(VueSplide)
