0dab6c28225b96569cdc004312f2e42d17912d2e
Marc Heiduk <marc.heiduk@geizhals.at>
2021-09-20 13:52:21 +0200

    add/replace socialmedia-icons

--- pages/footer.vue
+++ pages/footer.vue
@@ -11,14 +11,33 @@
 
           <div class="gh-footer__socialmedia-wrapper">
             <a href="#" target="_blank" class="gh-footer__socialmedia-item" aria-label="Facebook">
-              <svg class="gh-footer__socialmedia-icon" viewBox="0 0 512 512">
-                <path d="M455.27,32H56.73A24.74,24.74,0,0,0,32,56.73V455.27A24.74,24.74,0,0,0,56.73,480H256V304H202.45V240H256V189c0-57.86,40.13-89.36,91.82-89.36,24.73,0,51.33,1.86,57.51,2.68v60.43H364.15c-28.12,0-33.48,13.3-33.48,32.9V240h67l-8.75,64H330.67V480h124.6A24.74,24.74,0,0,0,480,455.27V56.73A24.74,24.74,0,0,0,455.27,32Z"/>
+              <svg xmlns="http://www.w3.org/2000/svg" class="gh-footer__socialmedia-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"></path>
               </svg>
             </a>
 
             <a href="#" target="_blank" class="gh-footer__socialmedia-item" aria-label="Twitter">
-              <svg class="gh-footer__socialmedia-icon" viewBox="0 0 512 512">
-                <path d="M496,109.5a201.8,201.8,0,0,1-56.55,15.3,97.51,97.51,0,0,0,43.33-53.6,197.74,197.74,0,0,1-62.56,23.5A99.14,99.14,0,0,0,348.31,64c-54.42,0-98.46,43.4-98.46,96.9a93.21,93.21,0,0,0,2.54,22.1,280.7,280.7,0,0,1-203-101.3A95.69,95.69,0,0,0,36,130.4C36,164,53.53,193.7,80,211.1A97.5,97.5,0,0,1,35.22,199v1.2c0,47,34,86.1,79,95a100.76,100.76,0,0,1-25.94,3.4,94.38,94.38,0,0,1-18.51-1.8c12.51,38.5,48.92,66.5,92.05,67.3A199.59,199.59,0,0,1,39.5,405.6,203,203,0,0,1,16,404.2,278.68,278.68,0,0,0,166.74,448c181.36,0,280.44-147.7,280.44-275.8,0-4.2-.11-8.4-.31-12.5A198.48,198.48,0,0,0,496,109.5Z"/>
+              <svg xmlns="http://www.w3.org/2000/svg" class="gh-footer__socialmedia-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z"></path>
+              </svg>
+            </a>
+
+            <a href="#" target="_blank" class="gh-footer__socialmedia-item" aria-label="Instagram">
+              <svg xmlns="http://www.w3.org/2000/svg" class="gh-footer__socialmedia-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <rect x="4" y="4" width="16" height="16" rx="4"></rect>
+                <circle cx="12" cy="12" r="3"></circle>
+                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501"></line>
+              </svg>
+            </a>
+
+            <a href="#" target="_blank" class="gh-footer__socialmedia-item" aria-label="YouTube">
+              <svg xmlns="http://www.w3.org/2000/svg" class="gh-footer__socialmedia-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <rect x="3" y="5" width="18" height="14" rx="4"></rect>
+                <path d="M10 9l5 3l-5 3z"></path>
               </svg>
             </a>
           </div>
@@ -132,23 +151,25 @@
       justify-center
       lg:justify-start
       mt-4
-      mx-0
-      lg:-mx-2
   }
 
   .gh-footer__socialmedia-item {
     @apply
-      mx-4
-      lg:mx-2
+      border
+      border-gray-400
+      rounded-full
+      p-2
+      mr-2
+      last:mr-0
   }
 
   .gh-footer__socialmedia-icon {
     @apply
-      w-8
-      h-8
-      lg:w-4
-      lg:h-4
-      fill-current
+      md:w-6
+      md:h-6
+      w-7
+      h-7
+      text-current
   }
 
   .gh-footer__link {
