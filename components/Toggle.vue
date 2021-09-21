9ebe67a3a9ba670ba7292f87792f163ccaf5d564
Marc Heiduk <marc.heiduk@geizhals.at>
2021-09-21 14:24:41 +0200

    own classes for 'toggle' component

--- components/Toggle.vue
+++ components/Toggle.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="inline-block border border-gray-300 rounded-lg p-4 mt-8">
-    <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+  <div class="gh-toggle">
+    <div class="gh-toggle__inner">
       <input
         type="checkbox"
         name="toggle"
         id="toggle"
-        class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 border-gray-300 appearance-none cursor-pointer"
+        class="gh-toggle-checkbox"
         :checked="active"
         @change="toggleOnChange"
       />
-      <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+      <label for="toggle" class="gh-toggle-label"></label>
     </div>
-    <label for="toggle" class="text-xs text-gray-700">{{text || 'Toggle me'}}</label>
+    <label for="toggle" class="gh-toggle-label-text">{{text || 'Toggle me'}}</label>
   </div>
 </template>
 
@@ -33,13 +33,66 @@
 </script>
 
 <style>
-  .toggle-checkbox:checked {
+  .gh-toggle {
+    @apply
+      inline-block
+      border
+      border-gray-300
+      rounded-lg
+      p-4
+      mt-8
+  }
+
+  .gh-toggle__inner {
+    @apply
+      relative
+      inline-block
+      w-10
+      mr-2
+      align-middle
+      select-none
+      transition
+      duration-200
+      ease-in
+  }
+
+  .gh-toggle-checkbox {
+    @apply
+      absolute
+      block
+      w-6
+      h-6
+      rounded-full
+      bg-white
+      border-4
+      border-gray-300
+      appearance-none
+      cursor-pointer
+  }
+
+  .gh-toggle-label {
+    @apply
+      block
+      overflow-hidden
+      h-6
+      rounded-full
+      bg-gray-300
+      cursor-pointer
+  }
+
+  .gh-toggle-label-text {
+    @apply
+      text-xs
+      text-gray-700
+  }
+
+  .gh-toggle-checkbox:checked {
     @apply
       right-0
       border-green-400
   }
 
-  .toggle-checkbox:checked + .toggle-label {
+  .gh-toggle-checkbox:checked + .gh-toggle-label {
     @apply
       bg-green-400
   }
